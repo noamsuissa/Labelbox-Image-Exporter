96eb3db41ed634db8f04cf2078e0297ef86955e9
noamsuissa <noam.suissa01@gmail.com>
2022-10-03 12:54:19 -0400

    test and train set exporters

--- test_exporter.py
+++ test_exporter.py
@@ -3,11 +3,11 @@ import json
 import os
 import requests
 
-dataset_name = "PECS_dataset"
-train_dir = "train"
+dataset_name = "Popliteal_dataset"
+test_dir = "test"
 images_dir = "images"
 masks_dir = "masks"
-export_path = "/Users/noamsuissa/Downloads/"
+export_path = "C:\\Users\\Noam Suissa\\Downloads"
 
 def download_image(pic_url, path):
     with open(path, 'wb') as handle:
@@ -25,7 +25,7 @@ def download_image(pic_url, path):
 #create dirs
 path = os.path.join(export_path, dataset_name)
 
-path2 = os.path.join(path, train_dir)
+path2 = os.path.join(path, test_dir)
 
 path3 = os.path.join(path2, images_dir)
 
@@ -38,14 +38,14 @@ if not os.path.exists(path):
 	os.mkdir(path4)
 
 #open labelbox json
-fp = open("/Users/noamsuissa/Downloads/rectus_labels.json")
+fp = open("C:\\Users\\Noam Suissa\\Downloads\\Popliteal_test_consensus.json")
 
 jsn = json.load(fp)
 
 #populate dirs
 for i in jsn:
 	
-	id_ = i["ID"]
+	id_ = i["DataRow ID"]
 	original_img_url = i["Labeled Data"]
 	objects = i["Label"]["objects"]
 
@@ -58,11 +58,26 @@ for i in jsn:
 	if not os.path.exists(im_path):
 		os.mkdir(im_path)
 
+	labeler = i["Created By"].split('@')[0]
+
+	if labeler == "kes401":
+		labeler = "Dr.Moghrabi"
+	if labeler == "ningnanw":
+		labeler = "Dr.Wang"
+	if labeler == "pascal.laferriere-langlois":
+		labeler = "Dr.Godbout-Simard"
+	if labeler == "pllanglois":
+		labeler = "Dr.Laferriere-Langlois"
+
+	labeler_path = os.path.join(im_path, labeler)
+	if not os.path.exists(labeler_path):
+			os.mkdir(labeler_path)
+
 	#download masks
 	for o in objects:
 		name = o["value"]
 		link = o["instanceURI"]
-		ob_path = os.path.join(im_path, name)
+		ob_path = os.path.join(labeler_path, name)
 		
 		if not os.path.exists(ob_path):
 			os.mkdir(ob_path)
